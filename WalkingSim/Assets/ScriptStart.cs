d0f6fd91766a1ff2fbbcd6d3dae1da475516e5db
Yuu-NiKorn <yuuyunikorn@gmail.com>
2025-11-20 15:06:26 +0100

    Ajj Ui start fonctionnel

---
+++ WalkingSim/Assets/ScriptStart.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Yuu
+{
+    public class ButtonStart : MonoBehaviour
+    {
+        public void OnStartClick()
+        {
+            Debug.Log("Bouton Start Appuyé");
+            SceneManager.LoadScene("RenduAuPropreSalles");
+        }
+
+        public void OnExitClick()
+        {
+#if UNITY_EDITOR
+            Debug.Log("all good");
+            // quitte playmode quand tu test dans editor
+            UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+        Application.Quit();
+#endif
+        }
+    }
+    
+}
